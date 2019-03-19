8f1fe01d5d342b199349a3a48652406225799bac
johnjonfenomenon <48635135+johnjonfenomenon@users.noreply.github.com>
2019-03-19 17:07:37 -0400

    Update bootstrap.js

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,12 +1,22 @@
-'use strict';
+var mysql = require('mysql');
 
-/**
- * An asynchronous bootstrap function that runs before
- * your application gets started.
- *
- * This gives you an opportunity to set up your data model,
- * run jobs, or perform some special logic.
- */
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "test_admin",
+  password: "pa$$word!",
+  database: "data.db"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO restaurants (name, description) VALUES ('The Bootstrap', 'Bootstrap.js restaurant entry')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+
 
 module.exports = cb => {
   cb();
